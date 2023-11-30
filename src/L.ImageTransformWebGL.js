11341ec2a518d832522769587c5d9af158732cfb
OriginalSin <serg@auchat.ru>
2023-11-30 20:37:51 +0300

    upd

--- src/L.ImageTransformWebGL.js
+++ src/L.ImageTransformWebGL.js
@@ -52,13 +52,14 @@ var ext = L.extend({
     },
 
     _onImageLoad: function () {
-		if (this._imgNode.decode) {
-			this._imgNode.decode({notifyWhen: 'paintable'})		// {firstFrameOnly: true}
-				.then(L.bind(this._imageReady, this))
-				.catch(function (ev) {
-					throw new Error(ev);
-				});
-		}
+		this._imageReady();
+		// if (this._imgNode.decode) {
+			// this._imgNode.decode({notifyWhen: 'paintable'})		// {firstFrameOnly: true}
+				// .then(L.bind(this._imageReady, this))
+				// .catch(function (ev) {
+					// throw new Error(ev);
+				// });
+		// }
     },
 
     _imageReady: function () {
@@ -69,17 +70,17 @@ var ext = L.extend({
     },
 
     _initImage: function () {
-		L.gmx.workerPromise.then(function() {
-			if (L.gmx.getBitmap) {
-				L.gmx.getBitmap(this._url).then(function(ev) {
-					if (ev.imageBitmap) {
-						this._imgNode = ev.imageBitmap;
-						this._imageReady();
-					} else {
-						console.warn('bitmap not found: ', this._url);
-					}
-				}.bind(this));
-			} else {
+		// L.gmx.workerPromise.then(function() {
+			// if (L.gmx.getBitmap) {
+				// L.gmx.getBitmap(this._url).then(function(ev) {
+					// if (ev.imageBitmap) {
+						// this._imgNode = ev.imageBitmap;
+						// this._imageReady();
+					// } else {
+						// console.warn('bitmap not found: ', this._url);
+					// }
+				// }.bind(this));
+			// } else {
 				this._imgNode = L.DomUtil.create('img', 'gmxImageTransform');
 				L.extend(this._imgNode, {
 					galleryimg: 'no',
@@ -89,8 +90,8 @@ var ext = L.extend({
 					onerror: L.bind(this._onImageError, this),
 					src: this._url
 				});
-			}
-		}.bind(this));
+			// }
+		// }.bind(this));
 
         this._image = L.DomUtil.create('div', 'leaflet-image-layer');
 		this._image.appendChild(this._canvas);
@@ -423,6 +424,71 @@ L.ImageTransformWebGL = L.ImageOverlay.extend(ext);
 L.imageTransformWebGL = function (url, bounds, options) {
 	return new L.ImageTransformWebGL(url, bounds, options);
 };
+//Based on http://math.stackexchange.com/questions/296794/finding-the-transform-matrix-from-4-projected-points-with-javascript
+(function() {
+    function adj(m) { // Compute the adjugate of m
+        return [
+            m[4] * m[8] - m[5] * m[7], m[2] * m[7] - m[1] * m[8], m[1] * m[5] - m[2] * m[4],
+            m[5] * m[6] - m[3] * m[8], m[0] * m[8] - m[2] * m[6], m[2] * m[3] - m[0] * m[5],
+            m[3] * m[7] - m[4] * m[6], m[1] * m[6] - m[0] * m[7], m[0] * m[4] - m[1] * m[3]
+        ];
+    }
+
+    function multmm(a, b) { // multiply two matrices
+        var c = Array(9);
+        for (var i = 0; i !== 3; ++i) {
+            for (var j = 0; j !== 3; ++j) {
+                var cij = 0;
+                for (var k = 0; k !== 3; ++k) {
+                    cij += a[3 * i + k] * b[3 * k + j];
+                }
+                c[3 * i + j] = cij;
+            }
+        }
+        return c;
+    }
+
+    function multmv(m, v) { // multiply matrix and vector
+        return [
+            m[0] * v[0] + m[1] * v[1] + m[2] * v[2],
+            m[3] * v[0] + m[4] * v[1] + m[5] * v[2],
+            m[6] * v[0] + m[7] * v[1] + m[8] * v[2]
+        ];
+    }
+
+    function basisToPoints(x1, y1, x2, y2, x3, y3, x4, y4) {
+        var m = [
+            x1, x2, x3,
+            y1, y2, y3,
+            1,  1,  1
+        ];
+        var v = multmv(adj(m), [x4, y4, 1]);
+        return multmm(m, [
+            v[0], 0, 0,
+            0, v[1], 0,
+            0, 0, v[2]
+        ]);
+    }
+L.ImageTransform = {};
+    L.ImageTransform.Utils = {
+        general2DProjection: function(
+              x1s, y1s, x1d, y1d,
+              x2s, y2s, x2d, y2d,
+              x3s, y3s, x3d, y3d,
+              x4s, y4s, x4d, y4d
+        ) {
+          var s = basisToPoints(x1s, y1s, x2s, y2s, x3s, y3s, x4s, y4s);
+          var d = basisToPoints(x1d, y1d, x2d, y2d, x3d, y3d, x4d, y4d);
+          return multmm(d, adj(s));
+        },
+
+        project: function(m, x, y) {
+            var v = multmv(m, [x, y, 1]);
+            return [v[0] / v[2], v[1] / v[2]];
+        },
+        adj: adj
+    };
+})();
 
 if (L.ImageTransform.Utils) {
 	L.gmx = L.gmx || {};
